2bd38e97d6dc5ed238f6952c5107be6e57fa9bd8
Md Rafiqul Islam Khan <mdrafiqulislamkhan@hotmail.com>
2019-09-29 18:17:57 +0600

    Initial Commit

---
+++ UpperLowerLimitIntegersBetweenThem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpperLowerLimitIntegersBetweenThem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Please insert the Upper Limit :");
+            int upperLimit = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(" Please insert the Lower Limit :");
+            int lowerLimit = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(" All Integers between them including them are given below :");
+
+
+            for (int i=upperLimit;i>=lowerLimit;i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();   
+
+        }
+    }
+}
